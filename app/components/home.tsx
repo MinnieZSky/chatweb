22746bc08e9bcfd1dc1cef28fd408d11aec8d51e
Dennis <zhwffy@hotmail.com>
2023-04-11 23:01:45 +0800

    Update home.tsx

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -119,9 +119,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT</div>
+          <div className={styles["sidebar-title"]}>AI 小助手</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            打造个人AI助理
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
@@ -157,7 +157,7 @@ function _Home() {
               />
             </div>
             <div className={styles["sidebar-action"]}>
-              <a href={REPO_URL} target="_blank">
+              <a href=""　target="_blank">
                 <IconButton icon={<GithubIcon />} shadow />
               </a>
             </div>
